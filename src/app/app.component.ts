c70e9529fb16072e46ee1787a1cad55f84efe8e2
HAREESH NALLA <hareesh.nalla@caliberhomeloans.com>
2022-01-02 20:11:25 -0600

    date range picker, preview edit design, copy animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cop-news-letter';
+  active = 'preview';
+  myControl = new FormControl();
+  options: string[] = ["Raghav's Template", 'Two', 'Three'];
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl(),
+  });
+
+  copyToClipboard() {
+    this.playAnimation();
+  }
+
+  playAnimation() {
+    var copyButton = document.getElementById(`copy-button-id`);
+    var copyAnimation = document.getElementById(`copy-animation-id`);
+
+    copyButton?.classList.add('jello-horizontal');
+    copyAnimation?.classList.add('copied-animation');
+    setTimeout(() => copyButton?.classList.remove('jello-horizontal'), 1000);
+    setTimeout(() => copyAnimation?.classList.remove('copied-animation'), 1900);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialExampleModule } from 'src/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
